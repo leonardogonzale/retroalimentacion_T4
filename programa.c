bc9e5f430b78ea70cf0cba106fee3fb8d73dfffd
Leonardo González <leonardo.gonzalez3077@alumnos.udg.mx>
2020-09-20 19:13:52 -0500

    AGREGUE EL TERCER CASE QUE ERA DE LA ESTRUCTURA PARA EL REGISTRO DE LOS PERSONAJES

--- programa.c
+++ programa.c
@@ -4,12 +4,18 @@
 #define t 5
 #define p printf
 
+void mostrar(int n, char arreglo[]);
 
+struct
+	{
+		char nombre[50],tipo[50];
+		float fuerza,salud;
+	}informacion[t];
 
 main()
 {
 	int i,n;
-	float arreglo[5];
+	char arreglo[20];
 	float prom,suma;
 	char opc,resp;
 	do{
@@ -40,7 +46,40 @@ main()
 					printf("el promedio de los numeros es: %.2f",prom);
 			break;
 			
-			
+			case 'b': case 'B':
+                printf("Escribe una cadena de 20 caracteres:  ");
+                fflush(stdin);
+                fgets(arreglo  , sizeof(arreglo), stdin);
+                printf("n: ");
+                scanf("%i", &n);
+                mostrar(n, arreglo);
+             break;
+
+			 case 'c': case 'C':
+			for(i=0; i<t; i++)
+				{
+					p("Ingrese el nombre del personaje:  ");
+					fflush(stdin);
+					gets(informacion[i].nombre);
+					p("Ingrese el tipo de clase del personaje:  ");
+					fflush(stdin);
+					gets(informacion[i].tipo);
+					p("Ingrese la fuerza del personaje (en numero):  ");
+					scanf("%f",&informacion[i].fuerza);
+					p("Ingrese la salud del personaje (total en numeros):  ");
+					scanf("%f",&informacion[i].salud);
+					p("\n");
+				}
+				for(i=0;i<t;i++)
+				{
+					p("\nNombre:  %s",informacion[i].nombre);
+					p("\nTipo de clase:  %s",informacion[i].tipo);
+					p("\nFuerza:  %.2f",informacion[i].salud);
+					p("\nSalud:  %.2f",informacion[i].fuerza);
+					p("\n");	
+				}
+				break;
+				default: printf("Opcion invalida\n");
 		}
 		fflush(stdin);
 		printf("\nDeseas volver a correr el programa?\n");
@@ -51,3 +90,13 @@ main()
 				
 }
 
+void mostrar(int n, char arreglo[])
+{ 
+	
+		for(size_t i = 0 ;i < n; i++)
+		{
+		
+			printf("%s\n",arreglo);	
+		}
+		
+}
